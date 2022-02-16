50cae8c656f58edd2b97abb058e109b3d6465679
JaretWright <jaret.wright@georgiancollege.ca>
2022-02-16 08:49:55 -0500

    Added view and controller for displaying a hand of cards

---
+++ src/main/java/com/example/w22comp1008lhw3/DealHandOfCardsController.java
@@ -0,0 +1,15 @@
+package com.example.w22comp1008lhw3;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class DealHandOfCardsController {
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private ImageView deckImageView;
+
+}
--- src/main/java/com/example/w22comp1008lhw3/Main.java
+++ src/main/java/com/example/w22comp1008lhw3/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("card-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("deal-hand-of-cards-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Card Viewer!");
         stage.setScene(scene);
